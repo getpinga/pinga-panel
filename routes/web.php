9ac703736a73b665c072e7fe59671aa53aa0212c
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-04 14:35:52 +0200

    Added light/dark mode

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,11 @@ $app->group('', function ($route) {
     $route->get('/reset-password', PasswordController::class.':resetPassword')->setName('reset.password');
     $route->get('/update-password', PasswordController::class.':createUpdatePassword')->setName('update.password');
     $route->post('/update-password', PasswordController::class.':updatePassword');
-
 })->add(new GuestMiddleware($container));
-
 
 $app->group('', function ($route) {
     $route->get('/dashboard', HomeController::class .':dashboard')->setName('home');
+    $route->get('/mode', HomeController::class .':mode')->setName('mode');
     $route->get('/logout', AuthController::class . ':logout')->setName('logout');
     $route->get('/change-password', PasswordController::class . ':createChangePassword')->setName('change.password');
     $route->post('/change-password', PasswordController::class . ':changePassword');
