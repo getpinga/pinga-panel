59cf453fd46a8ac5f6536191e3317ef05691b449
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-07 22:34:47 +0200

    update on profiles

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -13,7 +13,20 @@ class ProfileController extends Controller
 	{
 		$username = $_SESSION['auth_username'];
 		$email = $_SESSION['auth_email'];
-        return view($response,'admin/profile/profile.twig',['email' => $email, 'username' => $username]);
+		$status = $_SESSION['auth_status'];
+		if ($status == 0) {
+			$status = "Confirmed";
+		} else {
+			$status = "Unknown";
+		}
+		$roles = $_SESSION['auth_roles'];
+		if ($roles == 0) {
+			$role = "Admin";
+		} else {
+			$role = "Unknown";
+		}
+		
+        return view($response,'admin/profile/profile.twig',['email' => $email, 'username' => $username, 'status' => $status, 'role' => $role]);
 	}
 
 }
